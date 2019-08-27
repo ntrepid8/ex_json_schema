5d5f9cd5843a51e9437ee93b78234e57dcb3c9f5
Josh Austin <josh.austin@gmail.com>
2019-08-27 14:48:15 -0400

    fix deprecation warnings

--- lib/ex_json_schema/validator/properties.ex
+++ lib/ex_json_schema/validator/properties.ex
@@ -69,7 +69,9 @@ defmodule ExJsonSchema.Validator.Properties do
   end
 
   defp unvalidated_properties(properties, validated_properties) do
-    unvalidated = MapSet.difference(keys_as_set(properties), keys_as_set(validated_properties))
+    unvalidated =
+      MapSet.difference(keys_as_set(properties), keys_as_set(validated_properties))
+      |> MapSet.to_list()
     Map.take(properties, unvalidated)
   end
 
